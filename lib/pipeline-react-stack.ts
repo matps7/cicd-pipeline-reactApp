97566decfb956bc3da125dee46f386b4c1016db8
matteopascale <m.pascale@contrader,it>
2022-11-15 15:29:42 +0800

    ts update

--- lib/pipeline-react-stack.ts
+++ lib/pipeline-react-stack.ts
@@ -1,17 +1,31 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
+
 
 export class ReactPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'reactPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('KZNGroup/cicd-pipeline-react', 'main'),
-        commands: ['npm install', 'npm run build', 'npx cdk synth'],
-      }),
+    const branch = 'master';
+    const gitHubUsernameRepository = 'matps7/cicd-pipeline-reactApp';
+
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+        pipelineName: "MyCDKPipeline",
+        synth: new CodeBuildStep('SynthStep', {
+            input: CodePipelineSource.gitHub(gitHubUsernameRepository, branch, {
+                authentication: cdk.SecretValue.secretsManager('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'),
+            }),
+            installCommands: [
+                'npm install -g aws-cdk'
+            ],
+            commands: [
+                'npm install',
+                'npm ci',
+                'npm run build',
+                'npx cdk synth'
+            ]
+        })
     });
   }
 }
