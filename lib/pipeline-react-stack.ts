5d78eaf6c2fe5c8ec8d91fd81a1aa4d38c0ac902
matteopascale <m.pascale@contrader,it>
2022-11-17 16:10:05 +0800

    fix

--- lib/pipeline-react-stack.ts
+++ lib/pipeline-react-stack.ts
@@ -10,21 +10,14 @@ export class ReactPipelineStack extends cdk.Stack {
     const gitHubUsernameRepository = 'matps7/cicd-pipeline-reactApp';
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-        
         pipelineName: "MyCDKPipeline",
-        selfMutation: false,
         synth: new CodeBuildStep('SynthStep', {
             input: CodePipelineSource.connection('matps7/cicd-pipeline-reactApp', 'master', {
   connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:991679131068:connection/c64f0002-e4f9-428d-820f-1f8e0cddaad5'
             }),
-            installCommands: [
-                'npm install -g aws-cdk'
-            ],
             commands: [
-                'npm install',
                 'npm ci',
-                'npm run build',
-                'npx cdk synth'
+                'npm run build'
             ],
             buildEnvironment: {
               buildImage: cdk.aws_codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,
@@ -40,6 +33,10 @@ export class ReactPipelineStack extends cdk.Stack {
                     nodejs: "16"
                 }
             }
+        },
+        artifacts: {
+          "base-directory": "reactwebapp/build",
+          "files": "**/*"
         }
           })
         }
