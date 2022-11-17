4fe4cb8b71119a914e9d2015911d32d97df783ca
matteopascale <m.pascale@contrader,it>
2022-11-17 16:48:50 +0800

    f

--- lib/pipeline-react-stack.ts
+++ lib/pipeline-react-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodeBuildStep, CodePipeline, CodePipelineSource, } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource } from 'aws-cdk-lib/pipelines';
+
 
 export class ReactPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,14 +11,22 @@ export class ReactPipelineStack extends cdk.Stack {
     const gitHubUsernameRepository = 'matps7/cicd-pipeline-reactApp';
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
+        
         pipelineName: "MyCDKPipeline",
+        selfMutation: false,
         synth: new CodeBuildStep('SynthStep', {
             input: CodePipelineSource.connection('matps7/cicd-pipeline-reactApp', 'master', {
   connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:991679131068:connection/c64f0002-e4f9-428d-820f-1f8e0cddaad5'
             }),
+            
+            installCommands: [
+                'npm install -g aws-cdk'
+            ],
             commands: [
+                'npm install',
                 'npm ci',
-                'npm run build'
+                'npm run build',
+                'npx cdk synth'
             ],
             buildEnvironment: {
               buildImage: cdk.aws_codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,
@@ -27,16 +36,13 @@ export class ReactPipelineStack extends cdk.Stack {
             env:{
               'shell':'bash',
           },
+
           phases: {
             install: {
                 "runtime-versions": {
                     nodejs: "16"
                 }
             }
-        },
-        artifacts: {
-          "base-directory": "reactwebapp/build",
-          "files": "**/*"
         }
           })
         }
