6eaa15db550e271cfaf8363316e2cd7f3de2b6fa
matteopascale <m.pascale@contrader,it>
2022-11-18 09:48:49 +0800

    selfmutation:true

--- lib/pipeline-react-stack.ts
+++ lib/pipeline-react-stack.ts
@@ -12,7 +12,6 @@ export class ReactPipelineStack extends cdk.Stack {
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
         pipelineName: "MyCDKPipeline",
-        selfMutation: false,
         synth: new CodeBuildStep('SynthStep', {
             input: CodePipelineSource.connection('matps7/cicd-pipeline-reactApp', 'master', {
             connectionArn: 'arn:aws:codestar-connections:ap-southeast-2:991679131068:connection/c64f0002-e4f9-428d-820f-1f8e0cddaad5'
